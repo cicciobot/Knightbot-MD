53e8a37deb63d5428840b7fce0623ca7f97044f1
cicciobot <maryciccio254@gmail.com>
2025-05-10 10:28:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot‎',
   botName: "Knight Bot",
   botOwner: 'Knight Bot', // Your name
-  ownerNumber: '254712889487', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254716751015', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
